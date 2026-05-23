671932a024cdaa3f8a481e92c03bcd7385d21adc
jimsaku12 <53294881+jimsaku12@users.noreply.github.com>
2026-05-23 08:00:34 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from '../src/pages/HomePage.jsx';
+import HomePage from './pages/HomePage.jsx';
 import DetailPage from '../src/pages/DetailPage.jsx';
 
 const App = () => {
